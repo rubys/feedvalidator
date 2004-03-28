e88f449f81c966574edfca60fe902b94546a64e9
Joseph Walton <joe.walton.gglcd@googlemail.com>
2004-03-28 08:26:18 +0000

    Added code to detect XML character encoding outside the parser.
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@77 34a10a33-d82d-0410-ba40-81c901463ecc

---
+++ src/feedvalidator/xmlEncoding.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python
+
+"""
+$Id$
+This module deals with detecting XML encodings, using both BOMs and
+explicit declarations.
+"""
+
+__author__ = "Joseph Walton <http://www.kafsemo.org/>"
+__version__ = "$Revision$"
+__date__ = "$Date$"
+_copyright__ = "Copyright (c) 2004 Joseph Walton"
+__license__ = "Python"
+
+import codecs
+import re
+
+# These are generic decoders that are only used
+#  to decode the XML declaration, from which we can read
+#  the real encoding
+_decUCS4BE = codecs.getdecoder('UCS-4BE')
+_decUCS4LE = codecs.getdecoder('UCS-4LE')
+_decUCS2BE = codecs.getdecoder('UCS-2BE')
+_decUCS2LE = codecs.getdecoder('UCS-2LE')
+_decEBCDIC = codecs.getdecoder('IBM037') # EBCDIC
+_decACE = codecs.getdecoder('ISO-8859-1') # An ASCII-compatible encoding
+
+# Note that only the encodings in 'permitted' will be accepted.
+# This is so that, e.g., a 4-byte-character XML file that claims to be US-ASCII
+#  will fail now.
+def _decodeDeclaration(sig, dec, permitted):
+  encoding = _encodingFromDecl(dec(sig)[0])
+  if not(encoding):
+    from exceptions import UnicodeError
+    raise UnicodeError('This XML file requires an encoding declaration')
+  if permitted and not(encoding.upper() in permitted):
+    from exceptions import UnicodeError
+    raise UnicodeError('This XML file claims an encoding of ' + encoding + ', but looks more like ' + permitted[0])
+  return encoding
+
+def isStandard(x):
+  """ Is this encoding required by the XML 1.0 Specification, 4.3.3? """
+  return x.upper() in ['UTF-8', 'UTF-16']
+
+def isCommon(x):
+  """Is this encoding commonly used, according to
+  <http://www.syndic8.com/stats.php?Section=feeds#XMLEncodings>
+  (as of 2004-03-27)?"""
+
+  return isStandard(x) or x.upper() in ['US-ASCII', 'ISO-8859-1',
+    'EUC-JP', 'ISO-8859-2', 'ISO-8859-15', 'ISO-8859-7',
+    'KOI8-R', 'SHIFT_JIS', 'WINDOWS-1250', 'WINDOWS-1251',
+    'WINDOWS-1252', 'WINDOWS-1254', 'WINDOWS-1255', 'WINDOWS-1256',
+
+    # This doesn't seem to be popular, but is the Chinese
+    #  government's mandatory standard
+    'GB18030'
+    ]
+
+# Inspired by xmlproc's autodetect_encoding, but rewritten
+def detect(doc_start):
+  """This is the logic from appendix F.1 of the XML 1.0 specification.
+  Pass in the start of a document, and receive the encoding to use."""
+  sig = doc_start[:4]
+
+  # Too short to be anything else
+  if len(sig) < 4:
+    enc = 'UTF-8'
+  
+  # With a BOM. Use byte-order-neutral encoding
+  #  names, so the BOMs get stripped by the codecs
+  elif sig == '\x00\x00\xFE\xFF':  # UCS-4 BE
+    enc = 'UCS-4'
+  elif sig == '\xFF\xFE\x00\x00':  # UCS-4 LE
+    enc = 'UCS-4'
+  elif sig == '\x00\x00\xFF\xFE'  or sig == '\xFE\xFF\x00\x00':
+    raise 'Unable to process UCS-4 with unusual octet ordering'
+  elif sig[:2] == '\xFE\xFF':  # UTF-16 BE
+    enc = 'UTF-16'
+  elif sig[:2] == '\xFF\xFE':  # UTF-16 LE
+    enc = 'UTF-16'
+  elif sig[:3] == '\xEF\xBB\xBF':
+    enc = 'UTF-8'
+  
+  # Without a BOM; we read the declaration
+  elif sig == '\x00\x00\x00\x3C':
+    enc = _decodeDeclaration(doc_start, _decUCS4BE, ['UCS-4BE', 'UTF-32BE'])
+  elif sig == '\x3C\x00\x00\x00':
+    enc = _decodeDeclaration(doc_start, _decUCS4LE, ['UCS-4LE', 'UTF-32BE'])
+  elif sig == '\x00\x3C\x00\x3F':
+    enc = _decodeDeclaration(doc_start, _decUCS2BE, ['UTF-16BE', 'UCS-2BE'])
+  elif sig == '\x3C\x00\x3F\x00':
+    enc = _decodeDeclaration(doc_start, _decUCS2LE, ['UTF-16LE', 'UCS-2LE'])
+  elif sig == '\x3C\x3F\x78\x6D':
+    enc = _decodeDeclaration(doc_start, _decACE, None)
+  elif sig == '\x4C\x6F\xA7\x94':
+    enc = _decodeDeclaration(doc_start, _decEBCDIC, ['IBM037', 'CP037', 'IBM038', 'EBCDIC-INT'])
+  else:
+    # It's UTF-8, or mislabelled
+    enc = 'UTF-8'
+
+  return enc
+
+_encRe = re.compile(r'<\?xml\s+version\s*=\s*(?:"[-a-zA-Z0-9_.:]+"|\'[-a-zA-Z0-9_.:]+\')\s+(encoding\s*=\s*(?:"([-A-Za-z0-9._]+)"|\'([-A-Za-z0-9._]+)\'))')
+
+def _encodingFromDecl(x):
+  m = _encRe.match(x)
+  if m:
+    return m.group(2) or m.group(3)
+  else:
+    return None
+
+def removeDeclaration(x):
+  """Replace an XML document string's encoding declaration with the
+  same number of spaces. Some XML parsers don't allow the
+  encoding to be overridden, and this is a workaround."""
+  m = _encRe.match(x)
+  if m:
+    s = m.start(1)
+    e = m.end(1)
+    res = x[:s] + ' ' * (e - s) + x[e:]
+  else:
+    res = x
+  return res
+
+_encUTF8 = codecs.getencoder('UTF-8')
+
+def asUTF8(x):
+  """Accept a Unicode string and return a UTF-8 encoded string, with
+  its encoding declaration removed, suitable for parsing."""
+  x = removeEncoding(unicode(x))
+  return _encUTF8(x)[0]
+
+
+if __name__ == '__main__':
+  from sys import argv
+  from os.path import isfile
+
+  for x in argv[1:]:
+    if isfile(x):
+      f = open(x, 'r')
+      l = f.read(1024)
+      print x,detect(l)
+
+__history__ = """
+$Log$
+Revision 1.1  2004/03/28 08:26:18  josephw
+Added code to detect XML character encoding outside the parser.
+
+"""
