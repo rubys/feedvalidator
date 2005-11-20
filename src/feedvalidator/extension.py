be017fcc64b1151985ff012b6f929c6c19b015f9
Sam Ruby <rubys@intertwingly.net>
2005-11-20 01:39:50 +0000

    Add in country codes
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@367 34a10a33-d82d-0410-ba40-81c901463ecc

--- src/feedvalidator/extension.py
+++ src/feedvalidator/extension.py
@@ -697,7 +697,28 @@ class g_intUnit(text):
         "attr": ':'.join(self.name.split('_',1)), "value":self.value}))
 
 class iso3166(nonhtml):
-  pass # TODO: tbd
+  error = InvalidValue # TODO
+  valuelist = [
+    "AD", "AE", "AF", "AG", "AI", "AM", "AN", "AO", "AQ", "AR", "AS", "AT",
+    "AU", "AW", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ",
+    "BM", "BN", "BO", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC",
+    "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU",
+    "CV", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE",
+    "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", "GA",
+    "GB", "GD", "GE", "GF", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR",
+    "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HR", "HT", "HU", "ID",
+    "IE", "IL", "IN", "IO", "IQ", "IR", "IS", "IT", "JM", "JO", "JP", "KE",
+    "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB",
+    "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD",
+    "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT",
+    "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI",
+    "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH",
+    "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO",
+    "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK",
+    "SL", "SM", "SN", "SO", "SR", "ST", "SV", "SY", "SZ", "TC", "TD", "TF",
+    "TG", "TH", "TJ", "TK", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ",
+    "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN",
+    "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW"]
 
 class iso4217(enumeration):
   error = InvalidValue # TODO
@@ -724,6 +745,9 @@ class iso4217(enumeration):
 
 __history__ = """
 $Log$
+Revision 1.13  2005/11/20 01:39:50  rubys
+Add in country codes
+
 Revision 1.12  2005/11/20 00:36:00  rubys
 Initial support for gbase namespace
 
