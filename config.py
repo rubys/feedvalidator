c2dd709a75cf5686c7ed3b3da9ff0e5b28c10c30
Sam Ruby <rubys@intertwingly.net>
2006-03-04 13:31:37 +0000

    Update and link in the install howto
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@505 34a10a33-d82d-0410-ba40-81c901463ecc

--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 import os
 HOMEURL = 'http://' + os.environ['HTTP_HOST'] + os.environ['SCRIPT_NAME']
 
+# This is where the CGI itself is... other supporting scripts (like
+# feedfinder) may be placed here.
+WEBDIR = '/'.join(os.environ['SCRIPT_FILENAME'].split('/')[0:-1])
+
 # This following value is primarily used for setting up the other values...
-HOMEDIR = r'/home/rubys'
+HOMEDIR = WEBDIR
 
 # This is where local python libraries are installed.  This may be useful
 # for locating a locally installed libxml2 library, for example...
 PYDIR  = HOMEDIR + r'/lib/python/'
-
-# This is where the CGI itself is... other supporting scripts (like
-# feedfinder) may be placed here.
-WEBDIR = HOMEDIR + r'/public_html/feedvalidator'
 
 # This is where the feedvalidator code lives...
 SRCDIR = WEBDIR + r'/src'
