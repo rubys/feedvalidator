3973831166d6d8dcb3c1808e14150cf8dee1f253
Sam Ruby <rubys@intertwingly.net>
2006-03-27 23:19:36 +0000

    Add xmp and plaintext as security risks
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@587 34a10a33-d82d-0410-ba40-81c901463ecc

--- src/feedvalidator/validators.py
+++ src/feedvalidator/validators.py
@@ -74,12 +74,12 @@ class HTMLValidator(HTMLParser):
     "param", "pre", "q", "s", "samp", "script", "select", "small", "span",
     "strike", "strong", "style", "sub", "sup", "table", "tbody", "td",
     "textarea", "tfoot", "th", "thead", "title", "tr", "tt", "u", "ul",
-    "var", "xmp", "embed"]
+    "var", "xmp", "plaintext", "embed"]
   evilattrs = ['onabort', 'onblur', 'onchange', 'onclick', 'ondblclick',
                 'onerror', 'onfocus', 'onkeydown', 'onkeypress', 'onkeyup',
                 'onload', 'onmousedown', 'onmouseout', 'onmouseover',
                  'onmouseup', 'onreset', 'onresize', 'onsubmit', 'onunload']
-  eviltags = ['script','meta','embed','object','noscript'] 
+  eviltags = ['script','meta','embed','object','noscript','xmp','plaintext'] 
   def __init__(self,value):
     self.scripts=[]
     self.messages=[]
