14812fca59f3d66993e1a2fd6ca6cc9e5a4659f7
Sam Ruby <rubys@intertwingly.net>
2006-09-09 10:58:48 +0000

    Restore inadvertently deleted line
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@679 34a10a33-d82d-0410-ba40-81c901463ecc

--- src/demo.py
+++ src/demo.py
@@ -48,6 +48,7 @@ if __name__ == '__main__':
   from feedvalidator.formatter.text_plain import Formatter
   output = Formatter(events)
   if output:
+      print "\n".join(output)
       sys.exit(1)
   else:
       print "No errors or warnings"
