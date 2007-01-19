c6e8094566e7962f9096c34da550ada62c4682e8
Sam Ruby <rubys@intertwingly.net>
2007-01-19 23:35:50 +0000

    Remove Netscape deprecation warning
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@722 34a10a33-d82d-0410-ba40-81c901463ecc

--- src/feedvalidator/logging.py
+++ src/feedvalidator/logging.py
@@ -296,7 +296,7 @@ class ValidCopyright(ValidValue): pass
 class ValidGeneratorName(ValidValue): pass
 class OptionalValueMissing(ValidValue): pass
 class ValidDoctype(ValidValue): pass
-class DeprecatedDTD(Warning): pass
+class DeprecatedDTD(Info): pass
 class ValidHtml(ValidValue): pass
 class ValidAtomLinkRel(ValidValue): pass
 class ValidLatitude(ValidValue): pass
