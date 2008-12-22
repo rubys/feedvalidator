23fdf9b71471503b1cf90d5cfcf505c10429ca69
Joseph Walton <joe.walton.gglcd@googlemail.com>
2008-12-22 01:51:04 +0000

    Include setup script from Derek Richardson.
    git-svn-id: http://feedvalidator.googlecode.com/svn/trunk@1035 34a10a33-d82d-0410-ba40-81c901463ecc

---
+++ src/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = ''
+
+setup(name='feedvalidator',
+      version=version,
+      description="Validator for feeds",
+      long_description= \
+      """Feedvalidator validates feeds in a variety of syndication formats.
+      """,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='syndication atom rdf rss feeds',
+      author='Sam Ruby',
+      url='http://feedvalidator.org/',
+      license='MIT',
+      packages=['feedvalidator'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
