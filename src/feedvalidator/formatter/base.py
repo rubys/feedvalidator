1ca215b6c386d7db88d2cf80ec1082998b03bbe0
Sam Ruby <rubys@intertwingly.net>
2011-07-28 11:50:06 -0400

    LANGUAGE now looks like "en_US:en" on Ubuntu 11.04; adapt

--- src/feedvalidator/formatter/base.py
+++ src/feedvalidator/formatter/base.py
@@ -8,7 +8,7 @@ __copyright__ = "Copyright (c) 2002 Sam Ruby and Mark Pilgrim"
 
 from UserList import UserList
 import os
-LANGUAGE = os.environ.get('LANGUAGE', 'en')
+LANGUAGE = os.environ.get('LANGUAGE', 'en').split(':')[-1]
 lang = __import__('feedvalidator.i18n.%s' % LANGUAGE, globals(), locals(), LANGUAGE)
 
 from feedvalidator.logging import Info, Warning, Error
